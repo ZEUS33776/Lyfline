3cdd639dc621f6062a459cfb606f5361cb6e1a62
Arjun <arjunrd04@gmail.com>
2025-05-28 22:58:08 +0530

    fixed 404(i)

--- LyflineFrontend/vite.config.js
+++ LyflineFrontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,14 @@ export default defineConfig({
       output: {
         manualChunks: undefined,
       },
+    },
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
     },
   },
 })
